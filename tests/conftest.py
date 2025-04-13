b4096b4fb41652890fe044b35ac10847f4814065
Rodion Philippov <rodik.rahmanov@gmail.com>
2025-04-13 23:34:29 +0300

    Main program 3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from selene import browser
+from selenium.webdriver.chrome import webdriver
+
 
 @pytest.fixture(scope='function')
 def browser_options():
@@ -7,6 +9,7 @@ def browser_options():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+    driver_options = webdriver.ChromeOptions()
 
     yield
 
