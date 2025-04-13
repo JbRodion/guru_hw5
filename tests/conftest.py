981e01c7b66116e348888a1b7a696f592c559b78
Rodion Philippov <rodik.rahmanov@gmail.com>
2025-04-13 23:35:43 +0300

    Changes to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from selene import browser
-from selenium.webdriver.chrome import webdriver
 
 
 @pytest.fixture(scope='function')
@@ -9,7 +8,6 @@ def browser_options():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    driver_options = webdriver.ChromeOptions()
 
     yield
 
