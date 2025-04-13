a3b7ab168cd4c21b4f2393280ff8cbf51fb73a7a
Rodion Philippov <rodik.rahmanov@gmail.com>
2025-04-14 00:10:26 +0300

    Final commit, changes to state-city subject, hobby and picture

--- tests/test_guru_hw5.py
+++ tests/test_guru_hw5.py
@@ -1,5 +1,6 @@
 import os
-from selene import browser, be, have
+from selene import browser, be, have, command
+
 
 def test_form(browser_options):
     browser.open('/automation-practice-form')
@@ -21,25 +22,26 @@ def test_form(browser_options):
 
     #Date_of_birth
     browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').click().element('[value="11"]').click()
+    browser.element('.react-datepicker__month-select').click().element('[value="10"]').click()
     browser.element('.react-datepicker__year-select').click().element('[value="2001"]').click()
     browser.element('.react-datepicker__day--015').click()
 
     #Subjects
-    browser.element('#subjectsInput').click().type("E").press_enter()
+    browser.element('#subjectsInput').perform(
+        command.js.scroll_into_view).should(be.blank).type('Chemistry').press_enter()
 
     #Hobbies
-    browser.element('#hobbies-checkbox-3').click()
+    browser.element('[for=hobbies-checkbox-3]').click()
 
     #Picture
-    browser.element('#uploadPicture').click().send_keys(os.path.abspath('picture.png'))
+    browser.element('#uploadPicture').send_keys(os.path.abspath('picture.png'))
 
     #Address
     browser.element('#currentAddress').should(be.blank).click().type('Antalya')
 
     #State_City
-    browser.element('#react-select-3-input').click().press_enter()
-    browser.element('#react-select-4-input').click().press_enter()
+    browser.element('#state').click().element('#react-select-3-option-3').click()
+    browser.element('#city').click().element('#react-select-4-option-1').click()
 
     #Submit
     browser.element('#submit').click()
@@ -53,13 +55,12 @@ have.exact_texts('Label Values',
                          'Gender Male',
                          'Mobile 8987573856',
                          'Date of Birth 15 November,2001',
-                         'Subjects English',
+                         'Subjects Chemistry',
                          'Hobbies Music',
-                         'Picture picture.png.jpg',
+                         'Picture picture.png',
                          'Address Antalya',
-                         'State and City NCR Delhi',
+                         'State and City Rajasthan Jaiselmer',
                          )
     )
-browser.element('#closeLargeModal').should(be.clickable).click()
-
+    browser.element('#closeLargeModal').should(be.clickable)
 
